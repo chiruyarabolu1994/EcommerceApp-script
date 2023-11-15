97f01aa93fd5d61bf8ddf3b8a3712fc011a85c75
chiruyarabolu1994 <78079866+chiruyarabolu1994@users.noreply.github.com>
2023-11-15 20:26:53 +0530

    Update docker-script.sh

--- docker-script.sh
+++ docker-script.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-docker volume create ecommerce
-
 docker pull chiranjeeviy1994/ecommerceapp
 
-docker run -d -p 8080:8080 -v ecommerce/ROOT.war:/usr/local/tomcat/webapps/ROOT.war --name ecommerce chiranjeeviy1994/ecommerceapp
+docker run -d -p 8080:8080 --name ecommerce chiranjeeviy1994/ecommerceapp
